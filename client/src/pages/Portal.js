1f9dd5989f22c39533649c236e214c56e5c21201
Kel-D-Coder <okolochibundu887@gmail.com>
2024-12-26 23:16:15 +0100

    adjusted the customer portal route

--- client/src/pages/Portal.js
+++
@@ -1,7 +0,0 @@
-
-
-export const Portal = () => {
-    return (
-        <h1 className="text-white">Portal page</h1>
-    )
-}
--- server/controller/portal.controller.js
+++ server/controller/portal.controller.js
@@ -1,17 +1,20 @@
 const stripe = require("stripe")(process.env.STRIPE_SECRET);
 const http = require("http-status-codes");
+const User = require("../models/user.model.js");
 
 // ✅✅✅✅
 const CustomerPortal = async (req, res) => {
     try {
+        const user = await User.findById(req.userId).populate("subscription");
+
         const portalSession = await stripe.billingPortal.sessions.create({
-            customer: req.query.customer,
+            customer: user.subscription.customerId,
             return_url: 'http://localhost:3000/'
         })
 
         res.status(http.StatusCodes.OK).json({ portalUrl: portalSession.url});
     } catch (error) {
-        return res.status(http.StatusCodes.BAD_REQUEST).send();
+        return res.status(http.StatusCodes.BAD_REQUEST).json({ error: error.message });
     }
 }
 
